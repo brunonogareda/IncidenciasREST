0b524e7f7864ab85354b316157f78d8bf0abc74b
bruno.nogareda <brunonogareda@gmail.com>
2018-10-14 23:10:15 +0200

    Creación dun filtro para checkear a conexión coa base de datos.

--- src/main/java/es/brudi/incidencias/rest/util/Checkdb.java
+++ src/main/java/es/brudi/incidencias/rest/util/Checkdb.java
@@ -2,9 +2,7 @@ package es.brudi.incidencias.rest.util;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
-import javax.ws.rs.NameBinding;
 
-@NameBinding
 @Retention(RUNTIME)
 public @interface Checkdb {
 }
--- src/main/java/es/brudi/incidencias/rest/util/CheckdbFilter.java
+++ src/main/java/es/brudi/incidencias/rest/util/CheckdbFilter.java
@@ -2,8 +2,6 @@ package es.brudi.incidencias.rest.util;
 
 import java.io.IOException;
 
-import javax.annotation.Priority;
-import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.Response;
@@ -16,7 +14,6 @@ import es.brudi.incidencias.error.Error;
 
 @Checkdb
 @Provider
-@Priority(Priorities.AUTHORIZATION)
 public class CheckdbFilter implements ContainerRequestFilter {
 
 	private Logger logger = Logger.getLogger(CheckdbFilter.class);
